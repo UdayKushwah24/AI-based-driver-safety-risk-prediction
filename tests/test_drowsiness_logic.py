e1b14f8bd5a5d5dfde017c168f61b809db01fe5e
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-03-26 08:11:21 +0530

    eye coordinates in drowsiness test
    Added comments to clarify the coordinates used for open and closed eyes in the test.

--- tests/test_drowsiness_logic.py
+++ tests/test_drowsiness_logic.py
@@ -2,10 +2,12 @@ from backend.services.drowsiness_service import eye_aspect_ratio
 
 
 def test_ear_calculation_logic():
-    open_eye = [(0, 0), (1, 3), (2, 3), (4, 0), (2, 2), (1, 2)]
-    closed_eye = [(0, 0), (1, 1), (2, 1), (4, 0), (2, 1), (1, 1)]
+    open_eye = [(0, 0), (1, 3), (2, 3), (4, 0), (2, 2), (1, 2)]  # coordinates for open eyes
+    closed_eye = [(0, 0), (1, 1), (2, 1), (4, 0), (2, 1), (1, 1)]  # coordinates for closed eyes
 
+    
     ear_open = eye_aspect_ratio(open_eye)
+    
     ear_closed = eye_aspect_ratio(closed_eye)
 
     assert ear_open > ear_closed
